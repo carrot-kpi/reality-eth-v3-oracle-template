f50da375d17a7a84358ff222fa3540759f997011
Federico Luzzi <fedeluzzi00@gmail.com>
2023-02-22 17:21:04 +0000

    chore(frontend): update tailwind config

--- packages/frontend/tailwind.config.cjs
+++ packages/frontend/tailwind.config.cjs
@@ -1,8 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { join } = require("path");
+const { long: longCommitHash } = require("git-rev-sync");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    important: `#carrot-template-${longCommitHash(__dirname)}`,
     content: [
         join(__dirname, "./playground/**/*.{js,jsx,ts,tsx}"),
         join(__dirname, "./src/**/*.{js,jsx,ts,tsx}"),
