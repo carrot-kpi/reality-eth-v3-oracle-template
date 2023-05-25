a72fd6b3b736254041689105283792e2cc00870b
Federico Luzzi <fedeluzzi00@gmail.com>
2023-05-25 13:27:38 +0100

    feat(contracts): codegen script now always generates all possible variants of the contracts instead of one chain at a time

--- packages/contracts/codegen-chain-specific-contracts.js
+++ packages/contracts/codegen-chain-specific-contracts.js
@@ -7,38 +7,42 @@ import { ChainId } from "@carrot-kpi/sdk";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const SUPPORTED_CHAIN_IDS = [
+    ChainId.SEPOLIA,
+    ChainId.GNOSIS,
+    ChainId.SCROLL_TESTNET,
+];
+
 const REALITYV3_ADDRESS = {
     [ChainId.SEPOLIA]: "0x64a0745EF9d3772d9739D9350873eD3703bE45eC",
     [ChainId.GNOSIS]: "0xE78996A233895bE74a66F451f1019cA9734205cc",
-    [ChainId.SCROLL_TESTNET]: "0x4300d4C410f87c7c1824Cbc2eF67431030106604",
+    [ChainId.SCROLL_TESTNET]: "0xF2D17C08B6A3A60b5A32b95bC9621D292831446b",
 };
-
-const [, , chainId] = process.argv;
-
-if (!chainId) {
-    console.error("a chain id is required");
-    process.exit(1);
-}
-
-const realityV3Address = REALITYV3_ADDRESS[chainId];
-if (!realityV3Address) {
-    console.error(`unsupported chain id ${chainId}`);
-    process.exit(1);
-}
 
 const sources = ["RealityV3Oracle", "TrustedRealityV3Arbitrator"];
 
 const generate = async () => {
     for (const source of sources) {
-        let code = (
-            await readFile(join(__dirname, `./src/${source}.sol`))
-        ).toString();
-        code = code.replace(
-            "address(123456789)",
-            `address(${realityV3Address})`
-        );
+        for (const chainId of SUPPORTED_CHAIN_IDS) {
+            const realityV3Address = REALITYV3_ADDRESS[chainId];
+            if (!realityV3Address) {
+                console.error(`unsupported chain id ${chainId}`);
+                process.exit(1);
+            }
 
-        await writeFile(join(__dirname, `./src/${source}${chainId}.sol`), code);
+            let code = (
+                await readFile(join(__dirname, `./src/${source}.sol`))
+            ).toString();
+            code = code.replace(
+                "address(123456789)",
+                `address(${realityV3Address})`
+            );
+
+            await writeFile(
+                join(__dirname, `./src/${source}${chainId}.sol`),
+                code
+            );
+        }
     }
 };
 
