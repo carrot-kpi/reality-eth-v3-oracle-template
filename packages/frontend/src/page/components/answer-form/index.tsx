7520f085c740a0280ca55f1a56af48599a214b90
Paolo Guerra <pg.paologuerra@gmail.com>
2023-07-06 12:50:14 +0200

    Minor css issues (#84)
    * fix(frontend): minor css issues
    
    * fix(frontend): lint issue

--- packages/frontend/src/page/components/answer-form/index.tsx
+++ packages/frontend/src/page/components/answer-form/index.tsx
@@ -854,7 +854,9 @@ export const AnswerForm = ({
                     {loadingContent ? (
                         <Skeleton width="100px" />
                     ) : (
-                        <Markdown className={{ root: "font-medium" }}>
+                        <Markdown
+                            className={{ root: "font-medium overflow-x-auto" }}
+                        >
                             {content}
                         </Markdown>
                     )}
